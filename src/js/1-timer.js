1660eacec422c5d12b2287be169765ef10569de8
Mila <malaksumaaa@gmail.com>
2025-07-20 19:16:07 +0200

    add task timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -13,11 +13,6 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-iziToast.show({
-    title: 'Hey',
-    message: 'What would you like to add?'
-});
-
 
 export default defineConfig({
   optimizeDeps: {
@@ -25,41 +20,71 @@ export default defineConfig({
   },
 });
 
-    
+   const startBtn = document.querySelector('button[data-start]');
+const dataInput = document.querySelector('#datetime-picker');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+
+let userSelectedDate = null;
+startBtn.disabled = true;
+
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const dataNow = new Date();
+
+    if (selectedDate <= dataNow) {
+    
+      iziToast.warning({
+    title: 'Invalid date',
+    message: 'Please choose a date in the future',
+    position: 'topRight',
+    timeout: 3000,
+  });
+
+      startBtn.disabled = true;
+      userSelectedDate = null;
+
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    }
   },
 };
 
-flatpickr("#datetime-picker", options);
+flatpickr(dataInput, options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  return { days, hours, minutes, seconds };
-}
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+//   return { days, hours, minutes, seconds };
+// }
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 
 
