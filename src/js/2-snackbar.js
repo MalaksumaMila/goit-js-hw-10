05c48143a874d095f9a6dd907bf45d4882b6c0b7
Mila <malaksumaaa@gmail.com>
2025-07-21 22:36:45 +0200

    add snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -8,12 +8,37 @@ import "izitoast/dist/css/iziToast.min.css";
   title: 'Готово',
   message: '✅ Fulfilled promise in ${delay}ms',
   position: 'topRight',
-  timeout: this.delay,
+  timeout: 3000,
 });
 
 iziToast.error({
   title: 'Помилка',
   message: 'Rejected promise in ${delay}ms',
   position: 'topRight',
-  timeout: this.delay,
+  timeout: 3000,
 });
+
+const inputInterval = document.querySelector('input[type="number"]');
+const btn = document.querySelector('button');
+
+btn.addEventListener ("click", (element) => {
+    element.preventDefault();
+
+    const delay = Number(inputInterval.value);
+    const inputPromise = document.querySelector('input[name="state"]:checked');
+
+if (!delay || !inputPromise) {
+     iziToast.warning({
+      title: 'Увага',
+      message: 'Будь ласка, заповніть усі поля',
+      position: 'topRight',
+    });
+    return;
+}
+    
+const isFulfilled = inputPromise.value === "fulfilled";
+const isRejected = inputPromise.value === "rejected";
+
+
+
+})
