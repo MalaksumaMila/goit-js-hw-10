ffc2967382fd07c647ad3dfee7bfe796cb335a77
Mila <malaksumaaa@gmail.com>
2025-07-23 20:19:32 +0200

    add snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,26 +3,12 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-
-   iziToast.success({
-  title: 'Готово',
-  message: '✅ Fulfilled promise in ${delay}ms',
-  position: 'topRight',
-  timeout: 3000,
-});
-
-iziToast.error({
-  title: 'Помилка',
-  message: 'Rejected promise in ${delay}ms',
-  position: 'topRight',
-  timeout: 3000,
-});
-
+const form = document.querySelector('form')
 const inputInterval = document.querySelector('input[type="number"]');
 const btn = document.querySelector('button');
 
-btn.addEventListener ("click", (element) => {
-    element.preventDefault();
+form.addEventListener ("submit", (event) => {
+    event.preventDefault();
 
     const delay = Number(inputInterval.value);
     const inputPromise = document.querySelector('input[name="state"]:checked');
@@ -36,9 +22,34 @@ if (!delay || !inputPromise) {
     return;
 }
     
-const isFulfilled = inputPromise.value === "fulfilled";
-const isRejected = inputPromise.value === "rejected";
+const isPromise = inputPromise.value;
 
-
-
-})
+ new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isPromise === "fulfilled") {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  })
+    .then(() => {
+      console.log (`✅ Fulfilled promise in ${delay}ms`,)
+      iziToast.success({
+        title: 'Готово',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch(() => {
+      console.log (`❌ Rejected promise in ${delay}ms`
+)
+      iziToast.error({
+        title: 'Помилка',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+  });
